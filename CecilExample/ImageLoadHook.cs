17f29d4c9480bfc56648723a6d515ead49e6353b
khyperia <953151+khyperia@users.noreply.github.com>
2025-01-05 19:09:18 +0100

    brrr

--- CecilExample/ImageLoadHook.cs
+++ CecilExample/ImageLoadHook.cs
@@ -1,32 +1,21 @@
-﻿using Landfall.Modding;
+﻿using System.Runtime.CompilerServices;
+using Landfall.Modding;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
-using UnityEngine;
 
 namespace CecilExample;
 
 public static class ImageLoadHook
 {
-    public static void Hook(IntPtr imagePtr)
+    [ModuleInitializer]
+    public static void Hook()
     {
-        var image = new MonoImage(imagePtr);
-        if (!image.Storage.Key.Contains("Assembly-CSharp"))
-            return;
-        var resolver = new DefaultAssemblyResolver();
-        // add game's assembly directories to the resolve search path
-        resolver.AddSearchDirectory(Path.Join(Application.dataPath, "Managed"));
-        var readerParams = new ReaderParameters { AssemblyResolver = resolver };
-        using var stream = image.AssemblyDataStream;
-        var cecilAsm = AssemblyDefinition.ReadAssembly(stream, readerParams);
+        var cecilAsm = CecilPatcher.EditAssembly("Assembly-CSharp");
         var playerSlowMovement = cecilAsm.MainModule.GetType("PlayerSlowMovement");
         var body = playerSlowMovement.Methods.First(m => m.Name == "Jump").Body.GetILProcessor();
         var ldJumpForce = body.Body.Instructions.First(
             i => i.OpCode.Code == Code.Ldfld && i.Operand is FieldReference { Name: "jumpForce" });
         body.Remove(ldJumpForce.Previous); // remove load `this`
         body.Replace(ldJumpForce, body.Create(OpCodes.Ldc_R4, 100f));
-        using var memstream = new MemoryStream();
-        cecilAsm.Write(memstream);
-        image.OverwriteAssembly(memstream);
-        Debug.Log("CecilExample jump hook installed");
     }
 }
